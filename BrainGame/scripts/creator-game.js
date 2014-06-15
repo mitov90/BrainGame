477a8317464bc49f258aad1a6a70923df70cbb94
fast4y <dzhenko@gmail.com>
2014-06-15 22:01:04 +0300

    Final Push - fixed game options and didn't have time for a proper game over action :) Next time!

--- BrainGame/scripts/creator-game.js
+++ BrainGame/scripts/creator-game.js
@@ -1,1 +1,19 @@
-﻿
+﻿window.addEventListener('load', function () {
+    $('.help-content').prev().click(function () {
+        $('.help-content').toggle();
+    });
+
+    $('.start-game-menu-list').on('click', 'a', function (e) {
+        e.stopPropagation();
+        var option = $(this).attr('data-difficulty');
+        if (option) {
+            startGame(option);
+        }
+    });
+
+    function startGame(opt) {
+        $('#game-wrapper').css('background-image',"url('../images/GameBackground.png')").show();
+        $('#start-game-menu-container').remove();
+        createGame('#game-wrapper', opt);
+    }
+});
